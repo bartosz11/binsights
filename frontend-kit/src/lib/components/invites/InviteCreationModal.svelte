ad9f19c75f96ced0bbe9838c4511ca7e1e76c43a
bartosz11 <contact@bartosz.one>
2024-06-16 01:34:36 +0200

    Invite creation now uses a datetime input instead of unix epoch

--- frontend-kit/src/lib/components/invites/InviteCreationModal.svelte
+++ frontend-kit/src/lib/components/invites/InviteCreationModal.svelte
@@ -2,14 +2,16 @@
 	import http from '$lib/httpUtil';
 	import { error, promise } from '$lib/toastUtil';
 	import { closeModal } from 'svelte-modals';
-	import { number, required, useForm, validators } from 'svelte-use-form';
+	import { required, useForm, validators, type Validator } from 'svelte-use-form';
 
 	export let isOpen: boolean;
 
-	let expiryDate: number;
+	let expiryDate: string;
 
 	function onSubmit() {
-		promise(http.post(`/api/invite?expiryDate=${expiryDate}`), {
+		const expiryEpoch: number = new Date(expiryDate).getTime() / 1000;
+		console.log(expiryEpoch);
+		promise(http.post(`/api/invite?expiryDate=${expiryEpoch}`), {
 			success: 'Invite code created successfully.',
 			error: null,
 			loading: 'Creating an invite code...'
@@ -24,6 +26,11 @@
 	}
 
 	const form = useForm();
+	
+	const isFuture: Validator = (value) => {
+		// Don't call me a frontend developer - I cannot comprehend JS's stupidity and the fact that the comparison below works
+		return new Date(value) > new Date() ? null : { isFuture: 'Given date must be in the future' };
+	};
 </script>
 
 {#if isOpen}
@@ -32,12 +39,13 @@
 			<h3>Create a new invite</h3>
 			<form on:submit|preventDefault={onSubmit} use:form>
 				<div>
-					<label for="expiry">Expiry date (unix timestamp)</label>
+					<label for="expiry">Expiry date</label>
 					<input
 						name="expiry"
+						type="datetime-local"
 						placeholder="Expiry date"
 						bind:value={expiryDate}
-						use:validators={[required, number]}
+						use:validators={[required, isFuture]}
 						aria-invalid={$form.expiry?.touched
 							? $form.expiry.valid
 								? 'false'
@@ -48,8 +56,8 @@
 					{#if $form.expiry?.touched && $form.expiry?.errors.required}
 						<small id="expiry-hint">This field is required.</small>
 					{/if}
-					{#if $form.expiry?.touched && $form.expiry?.errors.number && !$form.expiry?.errors.required}
-						<small id="expiry-hint">Given value is not a number.</small>
+					{#if $form.expiry?.touched && $form.expiry?.errors.isFuture && !$form.expiry?.errors.required}
+						<small id="expiry-hint">Given value must be in the future.</small>
 					{/if}
 				</div>
 				<div role="group">
